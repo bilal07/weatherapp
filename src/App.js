6c8f7b64f1ce3b6f516b0fe03d4aef72bad0601a
bilal07 <mouadine.bilal@gmail.com>
2021-01-31 22:58:35 +0100

    get weather function using async await

--- src/App.js
+++ src/App.js
@@ -2,11 +2,46 @@ import React, { Component } from 'react';
 import Form from './components/Form';
 import Weather from './components/Weather'
 
+
+const API_KEY = "0e9c0128601738c96d5c69615fdff435";
+
 class App extends Component {
   
-  getWeather = (e) => {
+  state = {
+    temperature: '',
+    city: '',
+    country: '',
+    humidity: '',
+    description: '',
+    error: ''
+  }
+  getWeather = async (e) => {
     e.preventDefault();
-    console.log("weather");
+    const city = e.target.elements.city.value;
+    const country = e.target.elements.country.value;
+    
+    const api = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city}%2C${country}&appid=e36ed364400282e43250b6c4c0274d44`);
+    const data = await api.json();
+    
+    if(city & country) {
+      this.setState({
+        temperature: this.data.main.temp,
+        city: data.name,
+        country: data.sys.country,
+        humidity: data.main.humidity,
+        description: data.weather[0].desctription,
+        error: ''
+      })
+    } else {
+      this.setState({
+        temperature: '',
+        city: '',
+        country: '',
+        humidity: '',
+        description: '',
+        error: ''
+      })
+    }
   }
 
   render(){
